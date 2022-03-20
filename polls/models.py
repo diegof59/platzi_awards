9f2d661f9dd4fc3f5da7f3dbc7e38228d9ac7084
Diego Botero <diego.botero@correounivalle.edu.co>
2022-03-20 01:38:16 -0500

    CrInit django project. Create polls app. Create ER and relational data models.

---
+++ polls/models.py
@@ -0,0 +1,24 @@
+from django.utils import timezone
+from django.db import models
+import datetime
+
+# Create your models here.
+
+class Question(models.Model):
+    statement = models.CharField(max_length=200)
+    publication_date = models.DateTimeField("Date published")
+
+    def __str__(self) -> str:
+        return f'{self.statement}'
+    
+    def was_published_recently(self) -> bool:
+        """ Returns true if the question was published in the last day, else false """
+        return self.publication_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return
