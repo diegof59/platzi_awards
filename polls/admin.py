f2060ca12b999af40d4434e1df73cae2a3ae8615
Diego Botero <diego.botero@correounivalle.edu.co>
2022-03-20 23:53:34 -0500

    Tweaks to polls dj admin. Base template.

--- polls/admin.py
+++ polls/admin.py
@@ -2,14 +2,16 @@ from tabnanny import verbose
 from django.contrib import admin
 
 from .models import Question, Choice
-# Register your models here.
 
 class ChoiceInline(admin.StackedInline):
   model = Choice
   verbose_name_plurar = 'Choices'
-  can_delete = False
+  extra = 0
 
 class QuestionAdmin(admin.ModelAdmin):
   inlines = [ChoiceInline]
+  list_display = ["statement", "publication_date"]  
+  list_filter = ["publication_date"]
+  search_fields = ["statement"]
 
 admin.site.register(Question, QuestionAdmin)
